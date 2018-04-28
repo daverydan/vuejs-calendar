c96f04943dea1008fb9492368fe2c4e587291957
Danny Avery <danny.w.avery@gmail.com>
2018-04-28 13:27:07 -0600

    Add header + CurrentMonth.vue

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,5 +1,13 @@
 <template>
 	<div>
+		<div id="header">
+			<div>
+				<h1>Vue.js Calendar</h1>
+			</div>
+			<div>
+				<current-month></current-month>
+			</div>
+		</div>
 		<div id="day-bar">
 			<div>Mon</div>	
 			<div>Tue</div>	
@@ -20,9 +28,10 @@
 
 <script>
 	import CalendarDay from './CalendarDay.vue';
+	import CurrentMonth from './CurrentMonth.vue';
 
 	export default {
-		components: { CalendarDay },
+		components: { CalendarDay, CurrentMonth },
 
 		computed: {
 			month() { // get month from store
---
+++ src/components/CurrentMonth.vue
@@ -0,0 +1,9 @@
+<template>
+	<h3>Current Month</h3>
+</template>
+
+<script>
+	export default {
+
+	}
+</script>
