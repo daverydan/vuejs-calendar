7ed4a879a0056788d7d5c628ed1c582b6b1fb16e
Danny Avery <danny.w.avery@gmail.com>
2018-04-28 13:46:41 -0600

    Move Vuex store into own file

--- src/web.entry.js
+++ src/web.entry.js
@@ -1,6 +1,6 @@
 import Vue from 'vue';
 import moment from 'moment-timezone';
-import Vuex from 'vuex';
+import store from './store';
 
 import './style.scss';
 
@@ -10,24 +10,12 @@ import App from './components/App.vue';
 moment.tz.setDefault('UTC');
 Object.defineProperty(Vue.prototype, '$moment', { get() { return this.$root.moment } });
 
-Vue.use(Vuex);
-// my attempt
-// Object.defineProperty(Vue.prototype, '$vuex', { get() { return this.$root.vuex } });
-
 new Vue({
   el: '#app',
+  
+  store,
+  
   components: { App },
-  data: {
-  	moment
-  },
-  store: {
-	state: {
-		currentYear: 2018,
-		currentMonth: 4
-	}
-  },
-  created() {
-		// console.log(this.$moment);
-		// console.log(this.$vuex);
-	}
+  
+  data: { moment }
 });
